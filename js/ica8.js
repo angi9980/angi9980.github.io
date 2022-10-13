81894a6e381a5fd7ff3c02ff4259b4dd7e8e0282
angi9980 <112119148+angi9980@users.noreply.github.com>
2022-10-13 00:04:39 -0600

    ica8 added

---
+++ js/ica8.js
@@ -0,0 +1,12 @@
+let header = document.querySelector ('header');
+header.addEventListener('mouseover', changeColor);
+
+function changeColor() {
+    header.style.color = 'blue'
+}
+let img = img.querySelector('feature')
+img.addEventListener('click', changeImageSrc);
+
+function changeImageSrc() {
+    src = '../img/cat.gif.html'
+}
