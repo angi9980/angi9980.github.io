b3e6f66a10a6b8d142b17428b55e955c0c65ee41
angi9980 <112119148+angi9980@users.noreply.github.com>
2022-11-13 13:11:16 -0700

    updated assignment 12

--- js/wa12.js
+++ js/wa12.js
@@ -1,61 +1,33 @@
-const minusButton = document.querySelector(".minus-button").addEventListener("click", minus);
-const plusButton = document.querySelector(".plus-button").addEventListener("click", plusRunner);
-const resetButton = document.querySelector(".reset-button").addEventListener("click", reset);
-const randomButton = document.querySelector(".random-button").addEventListener("click", random);
-const submitButton = document.querySelector(".submit-button").addEventListener("click", submit);
+function check() {
+    console.log('test');
+}
+
+function submit() {
+    alert("Is this your number? " + output.textContent);
+}
+
+function reset() {
+    outputInt = "";
+    output.textContent = outputInt;
+}
+
+function roll() {
+    outputInt = randomNumber(0, 9);
+    let text = "Is this the next digit in your phone number?\nOK if yes, Cancel if no.\n" + outputInt;
+    if(confirm (text) === true){
+        output.textContent = output.textContent + outputInt;
+    }
+}
+
+function randomNumber(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+  }
 
 const output = document.querySelector('.output');
 let outputInt = parseInt(output.textContent);
-console.log(outputInt)
+console.log(outputInt);
 
-function check() {
-    console.log("test")
-}
-function submit() {
-    alert(output.textContent);
-}
-function reset() {
-    outputInt = 0;
-    output.textContent = outputInt;
-}
-function minus () {
-    if(outputInt > 1000000000) {
-    outputInt -=1;
-    output.textContent = outputInt;}
-}
-function plus() {
-    if (outputInt < 9999999999){
-    outputInt +=1;
-    output.textContent = outputInt;
-    }
-}
-function random() {
-    outputInt = randomNumber(0, 9999999999)
-    output.textContent = outputInt;
-}
-function randomNumber(){
-
-}
-function plusRunner(){
-    plus(outputInt);
-}
-
-
-/*const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-
-button.addEventListener('click', updateOutput);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value)
-} */
-
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener("click", update)
-var sliderOutput = document.querySelector(".slider-output");
-
-function update() {
-    sliderOutput.textContent = slider.value;
-}
+const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
+const randomButton = document.querySelector('.roll-button').addEventListener('click', roll);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
